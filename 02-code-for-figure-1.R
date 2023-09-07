12facaeb8700a1f78d8251a425833f0f6cc27b69
lmhagar <lmhagar@uwaterloo.ca>
2023-09-07 10:21:20 -0400

    upload data prep

---
+++ 02-code-for-figure-1.R
@@ -0,0 +1,212 @@
+## code to reproduce Figure 1 in the main text
+## this file should be run after "01-food-data-2020.R"
+
+## BEGIN SETUP ##
+
+## load necessary packages
+require(ggplot2)
+require(cowplot)
+require(rjags)
+require(coda)
+
+## load the .csv files created using "01-food-data-2020.R" 
+## save the food data in vector form
+y1 <- read.csv("aguas_food_F.csv")$food 
+y2 <- read.csv("aguas_food_M.csv")$food
+
+## save the data as data frame (for plotting later)
+foodF <- read.csv("aguas_food_F.csv")
+foodM <- read.csv("aguas_food_M.csv")
+
+## use JAGS to obtain posterior draws via MCMC
+set.seed(1)
+burnin <- 1000
+nchains <- 1
+nthin <- 2
+ndraws <- nthin*100000
+sum_y1 <- sum(y1)
+sum_logy1 <- sum(log(y1))
+n1 <- length(y1)
+mu0 <- 2; tau0 <- 0.25; kappa0 <- 2; lambda0 <- 0.25
+model1.fit <- jags.model(file="JAGS_gamma.txt",
+                         data=list(n=n1, sum_y = sum_y1, sum_logy = sum_logy1, 
+                                   tau0 = tau0, mu0 = mu0, zero = 0,
+                                   kappa0 = kappa0, lambda0 = lambda0), 
+                         n.chains = nchains)
+
+update(model1.fit, burnin)
+model1.samples <- coda.samples(model1.fit, c("alpha", "beta"), n.iter=ndraws, thin=nthin)
+
+alpha.1 <- unlist(model1.samples[,1])
+beta.1 <- unlist(model1.samples[,2])
+
+set.seed(2)
+sum_y2 <- sum(y2)
+sum_logy2 <- sum(log(y2))
+n2 <- length(y2)
+model2.fit <- jags.model(file="JAGS_gamma.txt",
+                         data=list(n=n2, sum_y = sum_y2, sum_logy = sum_logy2, 
+                                   tau0 = tau0, mu0 = mu0, zero = 0,
+                                   kappa0 = kappa0, lambda0 = lambda0), 
+                         n.chains = nchains)
+
+update(model2.fit, burnin)
+model2.samples <- coda.samples(model2.fit, c("alpha", "beta"), n.iter=ndraws, thin=nthin)
+
+alpha.2 <- unlist(model2.samples[,1])
+beta.2 <- unlist(model2.samples[,2])
+
+## obtain posterior of theta
+threshold <- 4.82
+theta.1 <- 1 - pgamma(threshold, alpha.1, beta.1)
+theta.2 <- 1 - pgamma(threshold, alpha.2, beta.2)
+theta <- theta.1/theta.2
+
+## find good histogram breaks based on the data
+breaks1 <- hist(y1, plot=FALSE)$breaks
+breaks2 <- hist(y2, plot=FALSE)$breaks
+binwidth2 <-0.64
+bins2 <- seq(0, 19.2, binwidth2)
+
+## create data frames for plotting
+x_fit1 <- seq(floor(qgamma(0.001,mean(alpha.1),mean(beta.1))),
+              ceiling(qgamma(0.999,mean(alpha.1),mean(beta.1))), by = 0.01)
+fit1 <- data.frame(xfit = x_fit1, yfit = dgamma(x_fit1,mean(alpha.1),mean(beta.1)))
+
+x_fit2 <- seq(floor(qgamma(0.001,mean(alpha.2),mean(beta.2))),
+              ceiling(qgamma(0.999,mean(alpha.2),mean(beta.2))), by = 0.01)
+fit2 <- data.frame(xfit = x_fit2, yfit = dgamma(x_fit2,mean(alpha.2),mean(beta.2)))
+
+hist_lower2 <- 0
+hist_upper2 <- 20
+
+group1_name <- "Female Household Provider"
+n1 <- length(y1)
+
+use_pctl <- 1
+metric_plot <- paste("Tail Probability", sep="")
+metric_value1 <- round(mean(ifelse(foodF$food >= 4.82,1,0)),3)
+percentile <- 0.9
+
+## make the plot for the female provider group data (top left corner)
+plot1a <-
+  ggplot(data=foodF, aes(foodF$food)) + theme_bw() +
+  geom_histogram(aes(y = (stat(count) / sum(count))/binwidth2), breaks = bins2,
+                 col="#1B86B4", 
+                 fill="#077DAA", 
+                 alpha = 0, size = 1) + 
+  coord_cartesian(xlim = c(hist_lower2, hist_upper2)) +
+  labs(title=paste(group1_name, "\n")) +
+  labs(x=bquote(atop(' ', atop(textstyle('Food Expenditure per Person (MXN $1000)'),
+                               textstyle('n = '*.(n1)*", "*.(metric_plot)*' ('*hat(theta)[1]*  
+                                           ') = '*.(metric_value1))))), y="Density\n") + 
+  theme(plot.title = element_text(hjust = 0.5,size=16,face="bold")) + 
+  theme(plot.subtitle = element_text(hjust = 0.5,size=14)) +
+  geom_area(data = fit1, aes(x=xfit, y=yfit), fill="#94D3FF", col="#077DAA", alpha=0.45, size = 1) +
+  geom_segment(aes(x = 4.82, y = 0, 
+                   xend=4.82, yend = (Inf*(use_pctl))), color="grey16", size=1.5) +
+  theme(axis.text=element_text(size=12),
+        axis.title=element_text(size=14)) + ylim(0, 0.3)
+
+group2_name <- "Male Household Provider"
+n2 <- length(y2)
+metric_value2 <- round(mean(ifelse(foodM$food >= 4.82,1,0)),3)
+
+## make the plot for the male provider group (bottom left corner)
+plot1c <- ggplot(data=foodM, aes(foodM$food)) + theme_bw() +
+  geom_histogram(aes(y = (stat(count) / sum(count))/binwidth2), breaks = bins2,
+                 col="#9E8203", 
+                 fill="#FED758", 
+                 alpha = 0, size = 1) + 
+  coord_cartesian(xlim = c(hist_lower2, hist_upper2)) +
+  labs(title=paste(group2_name,"\n")) +
+  labs(x=bquote(atop(' ', atop(textstyle('Food Expenditure per Person (MXN $1000)'),
+                               textstyle('n = '*.(n2)*", "*.(metric_plot)*' ('*hat(theta)[2]*  
+                                           ') = '*.(metric_value2))))), y="Density\n") + 
+  theme(plot.title = element_text(hjust = 0.5,size=16,face="bold")) +
+  theme(axis.text=element_text(size=12),
+        axis.title=element_text(size=14)) +
+  geom_area(data = fit2, aes(x=xfit, y=yfit), fill="#FED758", col="#9E8203", alpha=0.45, size = 1) + 
+  geom_segment(aes(x = 4.82, y = 0, 
+                   xend=4.82, yend = (Inf*(use_pctl))), color="grey16", size=1.5) + ylim(0, 0.3)
+
+## approximate the posterior using nonparametric density estimation
+d <- density(theta)
+d_frame <- data.frame(x = d$x, y = d$y)
+
+## make plot with no red shading first (additional layers to be added later)
+plot1e <- ggplot(data=d_frame, aes(x=d_frame$x)) + theme_bw() +
+  geom_polygon(aes(y=d_frame$y), col="gray10", fill="snow1", size=0.75, alpha=0.9) +
+  labs(title = bquote(bold("Posterior pdf of "~ theta[1] ~ '\u00F7' ~ theta[2]))) +
+  labs(subtitle = " ") +
+  labs(x=bquote(bold('\n Posterior pdf of'~ theta[1] ~ '\u00F7' ~ theta[2])), y=" ") + 
+  theme(plot.title = element_text(hjust = 0.5,size=18,face="bold", colour = "#FFFFFF")) +
+  theme(plot.subtitle = element_text(hjust = 0.5,size=16,face="bold")) +
+  theme(axis.text=element_text(size=12),
+        axis.title=element_text(size=16,face="bold")) + 
+  theme(axis.title.x = element_text(margin = unit(c(5, 0, 0, 0), "mm")))
+
+## calculate CPMs
+delta.L <- 1/1.1
+delta.U <- 1.1
+
+CPM_4 <- round(mean(ifelse(theta < delta.U, theta > delta.L,0)),4)
+CPM_title <- bquote(bold(italic(Pr)*'( '* 1.1^-1 < theta[1]~'\u00F7'~ theta[2] < 1.1*' | '*italic(data)*') =' ~ .(CPM_4)))
+
+x_title <- bquote(bold('\n Posterior pdf of'~ theta[1] ~ '\u00F7' ~ theta[2]))
+
+CPM_subset = subset(d_frame, x > delta.L & x < delta.U)
+x_new = 
+  c(max(delta.L,min(CPM_subset$x)), unlist(CPM_subset$x), 
+    min(delta.U,max(CPM_subset$x)))
+y_new = c(0,unlist(CPM_subset$y),0)
+CPM_frame = data.frame(x = x_new,y = y_new)
+
+## make plot for probability of agreement (top right corner)
+plot1b <- 
+  plot1e + labs(title = NULL) + labs(x = NULL) +
+  labs(title=CPM_title) +
+  labs(x= x_title) +
+  geom_polygon(data = CPM_frame, aes(x=x, y=y), fill="firebrick", col="gray10", size = 0.75, alpha = 0.75) +
+  theme(plot.title = element_text(hjust = 0.5,size=18,face="bold", colour = "#000000"))
+
+delta.L <- 1/1.1
+delta.U <- Inf
+
+CPM_4 <- round(mean(theta > delta.L),4)
+CPM_title <- bquote(bold(italic(Pr)*'( '* theta[1] > 1.1^-1*theta[2]*' | '*italic(data)*') =' ~ .(CPM_4)))
+
+x_title <- bquote(bold('\n Posterior pdf of'~ theta[1] ~ '\u00F7' ~ theta[2]))
+
+CPM_subset = subset(d_frame, x > delta.L & x < delta.U)
+x_new = 
+  c(max(delta.L,min(CPM_subset$x)), unlist(CPM_subset$x), 
+    min(delta.U,max(CPM_subset$x)))
+y_new = c(0,unlist(CPM_subset$y),0)
+CPM_frame = data.frame(x = x_new,y = y_new)
+
+## make plot for probability of noninferiority (bottom right corner)
+plot1d <- 
+  plot1e + labs(title = NULL) + labs(x = NULL) +
+  labs(title=CPM_title) +
+  labs(x= x_title) +
+  geom_polygon(data = CPM_frame, aes(x=x, y=y), fill="firebrick", col="gray10", size = 0.75, alpha = 0.75) +
+  theme(plot.title = element_text(hjust = 0.5,size=18,face="bold", colour = "#000000"))
+
+## combine into grid
+fig1.row1 <- plot_grid(plot1a + theme(plot.margin=unit(c(0.5,0.5,0.5,0.5),"cm")), 
+                       plot1b + theme(plot.margin=unit(c(0.5,0.5,0.5,0.5),"cm")),
+                       rel_widths = c(1.45, 1.15))
+fig1.row2 <- plot_grid(plot1c + theme(plot.margin=unit(c(0.5,0.5,0.5,0.5),"cm")), 
+                       plot1d + theme(plot.margin=unit(c(0.5,0.5,0.5,0.5),"cm")),
+                       rel_widths = c(1.45, 1.15))
+fig1 <- plot_grid(fig1.row1, fig1.row2, nrow = 2)
+
+## output as .pdf file for the article
+pdf(file = "Figure1BA.pdf",   # The directory you want to save the file in
+    width = 11.7895, # The width of the plot in inches (12.41)
+    height = 10.165) # The height of the plot in inches (10.7)
+
+fig1
+
+dev.off()
---
+++ 03-design-values-priors.R
@@ -0,0 +1,308 @@
+## code to determine design values and informative priors
+## this procedure involves processing data from the ENIGH 2018 survey
+
+## BEGIN SETUP ##
+
+## Process to obtain data file from ENIGH website
+## Step 1: Go to this website: https://www.inegi.org.mx/programas/enigh/nc/2018/#Datos_abiertos
+## Step 2: Click the "Download CSV" button to download a .zip file
+## Step 3: Open .zip file
+## Step 4: Open the "conjunto_de_datos_concentradohogar_enigh_2018_ns" subfolder
+## Step 5: Open the "conjunto_de_datos" subfolder
+## Step 6: Extract the lone .csv file: "conjunto_de_datos_concentradohogar_enigh_2018_ns.csv" and move it to the working directory
+
+## read data set
+enigh <- read.csv("conjunto_de_datos_concentradohogar_enigh_2018_ns.csv")
+
+## create a numeric character for region
+enigh$region <- as.numeric(substr(as.character(enigh$ubica_geo),1,nchar(as.character(enigh$ubica_geo))-3))
+
+## keep only the households from Aguascalientes (Region = 1):
+aguas <- enigh[enigh$region %in% c(1),]
+
+## keep only columns of interest
+aguas <- aguas[c("region","factor","est_socio","educa_jefe", "tot_integ", "alimentos", "sexo_jefe")]
+
+## convert column titles to English
+names(aguas)[4] <- "education"
+names(aguas)[5] <- "total_people"
+names(aguas)[6] <- "total_food"
+names(aguas)[7] <- "sex"
+
+## remove households with 0 quarterly expenditure on food
+aguas_full <- aguas ## save all data for later
+aguas <- aguas[aguas$total_food > 0,]
+
+## keep only individuals with estimated socioeconomic class 2
+aguas <- aguas[aguas$est_socio ==2 ,]
+
+## create simplified weighting factor 
+aguas$factor2 <- round(aguas$factor/75)
+
+## repeat the observations according to new weighting factor
+aguas_long <- aguas[1,]
+for (i in 1:nrow(aguas)){
+  if (i %% 100 == 0){
+    print(i)
+  }
+  for (j in 1:aguas$factor2[i]){
+    aguas_long <- rbind(aguas_long, aguas[i,])
+  }
+}
+aguas_long <- aguas_long[-1,]
+
+## calculate food expense per person in thousands of pesos (adjusted for 2 percent inflation)
+aguas_long$food <- 0.001*aguas_long$total_food/aguas_long$total_people*(1.02)^2
+
+## split based on sex of main household provider
+aguas_F <- aguas_long[aguas_long$sex == 2,]
+aguas_M <- aguas_long[aguas_long$sex ==1,]
+
+## remove households with more than 20000 pesos per person per month;
+## this is three households with female providers, and 2 with male providers
+aguas_F <- subset(aguas_F, aguas_F$food <= 20)
+aguas_M <- subset(aguas_M, aguas_M$food <= 20)
+
+## save food expenditure data for both groups
+write.csv(aguas_F[c("food")], "aguas2018_food_F.csv", row.names = FALSE)
+write.csv(aguas_M[c("food")], "aguas2018_food_M.csv", row.names = FALSE)
+
+## find the median food expenditure per person in upper income household from same region
+aguas_soc4 <- aguas_full[aguas_full$est_socio == 4,]
+
+## exclude households with no quarterly food expenditure (none for this example)
+aguas_soc4 <- aguas_soc4[aguas_soc4$total_food > 0,]
+
+## calculate food expense per person in thousands of pesos (adjusted for 2 percent inflation)
+aguas_soc4$food <- 0.001*aguas_soc4$total_food/aguas_soc4$total_people*(1.02)^2
+
+food4_rep <- NULL
+for (i in 1:nrow(aguas_soc4)){
+  food4_rep <- c(food4_rep, rep(aguas_soc4$food[i], aguas_soc4$factor[i]))
+}
+
+## confirmation that this expense is 4.29 thousand pesos (design value for threshold)
+median(food4_rep)
+
+## we now obtain design values for alpha1, beta1, alpha2, and beta2
+## first load the .csv files just created
+## save the food data in vector form
+y1 <- read.csv("aguas2018_food_F.csv")$food 
+y2 <- read.csv("aguas2018_food_M.csv")$food
+
+set.seed(5)
+burnin <- 1000
+nchains <- 1
+nthin <- 2
+ndraws <- nthin*100000
+sum_y1 <- sum(y1)
+sum_logy1 <- sum(log(y1))
+n1 <- length(y1)
+mu0 <- 2; tau0 <- 0.25; kappa0 <- 2; lambda0 <- 0.25
+model1.fit <- jags.model(file="JAGS_gamma.txt",
+                         data=list(n=n1, sum_y = sum_y1, sum_logy = sum_logy1, 
+                                   tau0 = tau0, mu0 = mu0, zero = 0,
+                                   kappa0 = kappa0, lambda0 = lambda0), 
+                         n.chains = nchains)
+
+update(model1.fit, burnin)
+model1.samples <- coda.samples(model1.fit, c("alpha", "beta"), n.iter=ndraws, thin=nthin)
+
+alpha.1 <- unlist(model1.samples[,1])
+beta.1 <- unlist(model1.samples[,2])
+
+set.seed(4)
+sum_y2 <- sum(y2)
+sum_logy2 <- sum(log(y2))
+n2 <- length(y2)
+model2.fit <- jags.model(file="JAGS_gamma.txt",
+                         data=list(n=n2, sum_y = sum_y2, sum_logy = sum_logy2, 
+                                   tau0 = tau0, mu0 = mu0, zero = 0,
+                                   kappa0 = kappa0, lambda0 = lambda0), 
+                         n.chains = nchains)
+
+update(model2.fit, burnin)
+model2.samples <- coda.samples(model2.fit, c("alpha", "beta"), n.iter=ndraws, thin=nthin)
+
+alpha.2 <- unlist(model2.samples[,1])
+beta.2 <- unlist(model2.samples[,2])
+
+## find posterior means for design values
+mean(alpha.1) ## should be 2.11
+mean(beta.1) ## should be 0.69
+mean(alpha.2) ## should be 2.43
+mean(beta.2) ## should be 0.79
+
+## save posterior draws to .csv files for reference
+write.csv(alpha.1, "alpha1s_2018.csv", row.names = FALSE)
+write.csv(beta.1, "beta1s_2018.csv", row.names = FALSE)
+write.csv(alpha.2, "alpha2s_2018.csv", row.names = FALSE)
+write.csv(beta.2, "beta2s_2018.csv", row.names = FALSE)
+
+## now we inflate the variances of these approximately gamma
+## marginal posteriors to find informative priors for the numerical study
+
+## define factor by which to inflate the variance
+var_inf <- 10
+
+## consider alpha1
+## find the posterior mode and variance
+a1dens <- density(alpha.1, n = 1024, bw = "SJ")
+a1_mode <- a1dens$x[which.max(a1dens$y)]
+a1_var <- var(alpha.1)
+
+## solve system of equations to get desired gamma distribution
+b1_star <- (-a1_mode - sqrt(a1_mode^2 + 4*var_inf*a1_var))/(-2*var_inf*a1_var); b1_star
+a1_star <- 1 + a1_mode*b1_star; a1_star
+
+## should be GAMMA(34.23, 15.85)
+informs <- c(a1_star, b1_star)
+
+## consider beta1
+## find the posterior mode and variance
+a1dens <- density(beta.1, n = 1024, bw = "SJ")
+a1_mode <- a1dens$x[which.max(a1dens$y)]
+a1_var <- var(beta.1)
+
+## solve system of equations to get desired gamma distribution
+b1_star <- (-a1_mode - sqrt(a1_mode^2 + 4*var_inf*a1_var))/(-2*var_inf*a1_var); b1_star
+a1_star <- 1 + a1_mode*b1_star; a1_star
+
+## should be GAMMA(27.20, 38.15)
+informs <- rbind(informs, c(a1_star, b1_star))
+
+## consider alpha2
+## find the posterior mode and variance
+a1dens <- density(alpha.2, n = 1024, bw = "SJ")
+a1_mode <- a1dens$x[which.max(a1dens$y)]
+a1_var <- var(alpha.2)
+
+## solve system of equations to get desired gamma distribution
+b1_star <- (-a1_mode - sqrt(a1_mode^2 + 4*var_inf*a1_var))/(-2*var_inf*a1_var); b1_star
+a1_star <- 1 + a1_mode*b1_star; a1_star
+
+## should be GAMMA(105.31, 42.96)
+informs <- rbind(informs, c(a1_star, b1_star))
+
+## consider beta2
+## find the posterior mode and variance
+a1dens <- density(beta.2, n = 1024, bw = "SJ")
+a1_mode <- a1dens$x[which.max(a1dens$y)]
+a1_var <- var(beta.2)
+
+## solve system of equations to get desired gamma distribution
+b1_star <- (-a1_mode - sqrt(a1_mode^2 + 4*var_inf*a1_var))/(-2*var_inf*a1_var); b1_star
+a1_star <- 1 + a1_mode*b1_star; a1_star
+
+## should be GAMMA(85.49, 106.58)
+informs <- rbind(informs, c(a1_star, b1_star))
+
+## output informative gamma distribution hyperparameters to .csv file
+write.csv(informs, "informs_gamma.csv", row.names = FALSE)
+
+## repeat this process for the Weibull distribution
+set.seed(6)
+burnin <- 1000
+nchains <- 1
+nthin <- 2
+ndraws <- nthin*100000
+n1 <- length(y1)
+mu0 <- 2; tau0 <- 1; kappa0 <- 2; lambda0 <- 1
+model1.fit <- jags.model(file="JAGS_weibull.txt",
+                         data=list(n=n1, y = y1, 
+                                   tau0 = tau0, mu0 = mu0,
+                                   kappa0 = kappa0, lambda0 = lambda0), 
+                         n.chains = nchains)
+
+update(model1.fit, burnin)
+model1.samples <- coda.samples(model1.fit, c("nu", "l"), n.iter=ndraws, thin=nthin)
+
+nu.1 <- unlist(model1.samples[,2])
+lambda.1 <- unlist(model1.samples[,1])
+
+set.seed(7)
+n2 <- length(y2)
+model2.fit <- jags.model(file="JAGS_weibull.txt",
+                         data=list(n=n2, y = y2, 
+                                   tau0 = tau0, mu0 = mu0,
+                                   kappa0 = kappa0, lambda0 = lambda0), 
+                         n.chains = nchains)
+
+update(model2.fit, burnin)
+model2.samples <- coda.samples(model2.fit, c("nu", "l"), n.iter=ndraws, thin=nthin)
+
+nu.2 <- unlist(model2.samples[,2])
+lambda.2 <- unlist(model2.samples[,1])
+
+## find posterior means for design values
+mean(nu.1) ## should be 1.41
+mean(lambda.1) ## should be 3.39
+mean(nu.2) ## should be 1.49
+mean(lambda.2) ## should be 3.42
+
+## save posterior draws to .csv files for reference
+write.csv(nu.1, "nu1s_2018.csv", row.names = FALSE)
+write.csv(lambda.1, "lambda1s_2018.csv", row.names = FALSE)
+write.csv(nu.2, "nu2s_2018.csv", row.names = FALSE)
+write.csv(lambda.2, "lambda2s_2018.csv", row.names = FALSE)
+
+## now we inflate the variances of these approximately gamma
+## marginal posteriors to find informative priors for the numerical study
+
+## define factor by which to inflate the variance
+var_inf <- 100
+
+## consider nu1
+## find the posterior mode and variance
+a1dens <- density(nu.1, n = 1024, bw = "SJ")
+a1_mode <- a1dens$x[which.max(a1dens$y)]
+a1_var <- var(nu.1)
+
+## solve system of equations to get desired gamma distribution
+b1_star <- (-a1_mode - sqrt(a1_mode^2 + 4*var_inf*a1_var))/(-2*var_inf*a1_var); b1_star
+a1_star <- 1 + a1_mode*b1_star; a1_star
+
+## should be GAMMA(12.73,  8.28)
+informs <- c(a1_star, b1_star)
+
+## consider lambda1
+## find the posterior mode and variance
+a1dens <- density(lambda.1, n = 1024, bw = "SJ")
+a1_mode <- a1dens$x[which.max(a1dens$y)]
+a1_var <- var(lambda.1)
+
+## solve system of equations to get desired gamma distribution
+b1_star <- (-a1_mode - sqrt(a1_mode^2 + 4*var_inf*a1_var))/(-2*var_inf*a1_var); b1_star
+a1_star <- 1 + a1_mode*b1_star; a1_star
+
+## should be GAMMA(11.81,  3.20)
+informs <- rbind(informs, c(a1_star, b1_star))
+
+## consider nu2
+## find the posterior mode and variance
+a1dens <- density(nu.2, n = 1024, bw = "SJ")
+a1_mode <- a1dens$x[which.max(a1dens$y)]
+a1_var <- var(nu.2)
+
+## solve system of equations to get desired gamma distribution
+b1_star <- (-a1_mode - sqrt(a1_mode^2 + 4*var_inf*a1_var))/(-2*var_inf*a1_var); b1_star
+a1_star <- 1 + a1_mode*b1_star; a1_star
+
+## should be GAMMA(38.35, 25.09)
+informs <- rbind(informs, c(a1_star, b1_star))
+
+## consider lambda2
+## find the posterior mode and variance
+a1dens <- density(lambda.2, n = 1024, bw = "SJ")
+a1_mode <- a1dens$x[which.max(a1dens$y)]
+a1_var <- var(lambda.2)
+
+## solve system of equations to get desired gamma distribution
+b1_star <- (-a1_mode - sqrt(a1_mode^2 + 4*var_inf*a1_var))/(-2*var_inf*a1_var); b1_star
+a1_star <- 1 + a1_mode*b1_star; a1_star
+
+## should be GAMMA(37.91, 10.79)
+informs <- rbind(informs, c(a1_star, b1_star))
+
+## output informative weibull distribution hyperparameters to .csv file
+write.csv(informs, "informs_weibull.csv", row.names = FALSE)
